9783489f61f0131adfe7f327d466a40d69386fc4
Azabu-bound <tylerjbailey95@gmail.com>
2025-03-10 11:38:06 -0400

    adding test cases and test.sh file

--- src/textnode.py
+++ src/textnode.py
@@ -15,7 +15,7 @@ class TextNode():
         self.url = url
 
     def __eq__(self, other):
-        return(
+        return (
             self.text == other.text and
             self.text_type == other.text_type and
             self.url == other.url
